e748bc602fb42d36524a3bf72407fb19c6656b2a
Gabriel-Cosmin Rautu <rautu_cosmin21@yahoo.com>
2025-10-18 18:32:18 +0300

    check

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
-
+ 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
