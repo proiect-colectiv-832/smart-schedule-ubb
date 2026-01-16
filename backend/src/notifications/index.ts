a7eec5d1b8f22160fc23bfe0cf29490af834a62f
Maier Horațiu-Gabriel <maierhoratiu7@gmail.com>
2026-01-16 21:35:06 +0200

    reparat

---
+++ backend/src/notifications/index.ts
@@ -0,0 +1,5 @@
+// Notifications - Re-export all notification-related modules
+export * from './notification-manager';
+export * from './notification-types';
+export * from './push-notification-manager';
+
